fea33cef073b3968b38fe0e2476abac7c2e57af6
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-05-29 12:28:56 +0300

    fix typo

--- sync_brain.sh
+++ sync_brain.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-rsync -Pa --delete -e 'ssh -i /sdcard/brain/keys/hetzner1' mahmooz@192.168.1.188:/home/mahmoz/brain /sdcard --exclude '*.mp4' --exclude '*mail*'
+rsync -Pa --delete -e 'ssh -i /sdcard/brain/keys/hetzner1' --exclude '*.mp4' --exclude '*mail*' mahmooz@192.168.1.188:/home/mahmooz/brain /sdcard
