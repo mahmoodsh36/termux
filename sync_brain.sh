75640d0527d9bc6058e7a08f9444e22ceefe1e84
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-09-19 13:04:29 +0300

    update ip

--- sync_brain.sh
+++ sync_brain.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-rsync -Pa --delete -e 'ssh -i /sdcard/brain/keys/hetzner1' --exclude '*.mp4' --exclude '*mail*' mahmooz@192.168.1.188:/home/mahmooz/brain /sdcard
+rsync -Pa --delete -e 'ssh -i /sdcard/brain/keys/hetzner1' --exclude '*.mp4' --exclude '*mail*' mahmooz@192.168.1.238:/home/mahmooz/brain /sdcard
