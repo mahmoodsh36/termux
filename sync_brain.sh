23d197efd6e7beecd487de778cace62f196b91e2
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-09-26 21:44:52 +0300

    dont sync venv

--- sync_brain.sh
+++ sync_brain.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-rsync -Pa --delete -e 'ssh -i /sdcard/brain/keys/hetzner1' --exclude '*.mp4' --exclude '*mail*' mahmooz@192.168.1.100:/home/mahmooz/brain /sdcard
+rsync -Pa --delete -e 'ssh -i /sdcard/brain/keys/hetzner1' --exclude '*.mp4' --exclude '*venv*' --exclude '*mail*' mahmooz@192.168.1.100:/home/mahmooz/brain /sdcard
