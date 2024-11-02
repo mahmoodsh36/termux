ccca406d7776887e6efb1834c509f76ca5da815a
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-11-02 19:27:49 +0200

    use find_computers.py

--- sync_brain.sh
+++ sync_brain.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env sh
-rsync -Pa --delete -e 'ssh -i /sdcard/brain/keys/hetzner1' --exclude '*.mp4' --exclude '*venv*' --exclude '*mail*' mahmooz@192.168.1.100:/home/mahmooz/brain /sdcard
+# rsync -Pa --delete -e 'ssh -i /sdcard/brain/keys/hetzner1' --exclude '*.mp4' --exclude '*venv*' --exclude '*mail*' mahmooz@192.168.1.100:/home/mahmooz/brain /sdcard
+hostname=$(find_computers.py print_ips | head -1)
+ip=$(echo "$hostname" | cut -d ':' -f1)
+port=$(echo "$hostname" | cut -d ':' -f2)
+if [ -z "$port" ]; then
+    port=22
+fi
+if [ ! -z "$hostname" ]; then
+    rsync -Pa --delete -e "ssh -i /sdcard/brain/keys/hetzner1 -p $port" --exclude '*.mp4' --exclude '*venv*' --exclude '*mail*' mahmooz@"$ip":/home/mahmooz/brain /sdcard
+else
+    echo no server
+fi
