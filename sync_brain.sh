fb59190459c9c1890f20d5a0e473727ddd0641f1
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-11-04 09:42:26 +0200

    sync from main server

--- sync_brain.sh
+++ sync_brain.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
+
 # rsync -Pa --delete -e 'ssh -i /sdcard/brain/keys/hetzner1' --exclude '*.mp4' --exclude '*venv*' --exclude '*mail*' mahmooz@192.168.1.100:/home/mahmooz/brain /sdcard
-hostname=$(find_computers.py print_ips | head -1)
-ip=$(echo "$hostname" | cut -d ':' -f1)
-port=$(echo "$hostname" | cut -d ':' -f2)
-if [ -z "$port" ]; then
-    port=22
-fi
-if [ ! -z "$hostname" ]; then
-    rsync -Pa --delete -e "ssh -i /sdcard/brain/keys/hetzner1 -p $port" --exclude '*.mp4' --exclude '*venv*' --exclude '*mail*' mahmooz@"$ip":/home/mahmooz/brain /sdcard
-else
-    echo no server
-fi
+
+# hostname=$(find_computers.py print_ips | head -1)
+# ip=$(echo "$hostname" | cut -d ':' -f1)
+# port=$(echo "$hostname" | cut -d ':' -f2)
+# if [ -z "$port" ]; then
+#     port=22
+# fi
+# if [ ! -z "$hostname" ]; then
+#     rsync -Pa --delete -e "ssh -i /sdcard/brain/keys/hetzner1 -p $port" --exclude '*.mp4' --exclude '*venv*' --exclude '*mail*' mahmooz@"$ip":/home/mahmooz/brain /sdcard
+# else
+#     echo no server
+# fi
+
+rsync -Pa --delete -e 'ssh -i /sdcard/brain/keys/hetzner1 -p 5001' --exclude '*.mp4' --exclude '*venv*' --exclude '*mail*' mahmooz@95.217.0.99:/home/mahmooz/brain /sdcard
